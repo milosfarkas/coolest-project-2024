e50d957e2c465fd620e54191cd0abf0cdef76fe4
Milos Farkas <milosfarkas@users.noreply.github.com>
2024-10-25 19:33:52 +0200

    javitas

--- godot/scenes/player.gd
+++ godot/scenes/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -320.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animation = get_node("AnimationPlayer")
+@onready var animation = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
