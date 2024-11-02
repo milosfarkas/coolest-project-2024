e06595fd0ee06de6a4d2279c253348620d7920f0
Milos Farkas <milosfarkas@users.noreply.github.com>
2024-11-02 09:29:44 +0100

    kamera mozgas javitas

--- godot/scenes/player.gd
+++ godot/scenes/player.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 		animation_player.play("attack")
 	
 	# Handle Jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
