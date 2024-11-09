5bb7b18e7f0d248ea2667a3fbdb6265f3d7a9ad5
Milos Farkas <milosfarkas@users.noreply.github.com>
2024-11-09 20:41:15 +0100

    asset

---
+++ godot/mobile_control.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_buttons"):
+		visible = not visible
